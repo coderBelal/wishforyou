13ae05b3ef017343ad36852a7bf5d53a0f2f4873
Belal Uddin <139705476+coderBelal@users.noreply.github.com>
2024-07-09 17:22:13 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+const App = () => {
+  return (
+    <div className="birthdayCard">
+      <div className="cardFront">
+        <h3 className="happy">MY DEAR.......</h3>
+        <h4 className="happy" />
+        <div className="balloons">
+          <div className="balloon-1" />
+          <div className="balloon-2" />
+          <div className="balloon-3" />
+          <div className="balloon-4" />
+        </div>
+      </div>
+      <div className="cardInside">
+        <h3 className="back">....</h3>
+        <p>......</p>
+        <p>........</p>
+        <p className="name">........</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
