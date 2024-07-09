6577a0edda24f526ebbcd833448b5cba087fc829
Belal Uddin <139705476+coderBelal@users.noreply.github.com>
2024-07-09 17:34:54 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
       <div className="cardFront">
         <h3 className="happy">MY DEAR /h3>
           <h3 className="happy">Afia</h3>
-        <h4 className="happy" />
+       
         <div className="balloons">
           <div className="balloon-1" />
           <div className="balloon-2" />
@@ -14,7 +14,7 @@ const App = () => {
       </div>
       <div className="cardInside">
         <h3 className="back"></h3>
-        <p>Dear Afia</p>
+        <p>Dear</p>
         <p>
 I hope you're well. I wanted to tell you how grateful I am to have you as my best friend. Your support, kindness, and understanding mean the world to me. 
 Thank you for always being there, whether we’re sharing laughter or facing challenges together. You are truly special, and I cherish our friendship too deeply</p>
