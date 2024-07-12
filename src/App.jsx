7a5bd36335923afc7383ae07be9f3171704ee4f4
Belal Uddin <139705476+coderBelal@users.noreply.github.com>
2024-07-12 13:56:00 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ const App = () => {
     <div className="birthdayCard">
       <div className="cardFront">
         <h3 className="happy">MY DEAR </h3>
-          <h4 className="happy">Afia</h4>
+          <h4 className="happy">Hsan Bhaiyaaaa</h4>
        
         <div className="balloons">
           <div className="balloon-1" />
@@ -14,10 +14,14 @@ const App = () => {
       </div>
       <div className="cardInside">
         <h3 className="back"></h3>
-        <p>Dear Afia</p>
+        <p>Dear Hasan Bhaiyaaaaaa</p>
         <p>
-I hope you're well. I wanted to tell you how grateful I am to have you as my best friend. Your support, kindness, and understanding mean the world to me. 
-Thank you for always being there, whether we’re sharing laughter or facing challenges together. You are truly special, and I cherish our friendship too deeply</p>
+Happy Birthday! On this special day, I want to express how grateful I am to have you in my life. You have always been my support, my confidant, and my best friend. I cherish all the memories we've made together and look forward to many more.
+
+I hope this year brings you everything you wish for and more. May you achieve all your dreams and find happiness in every step you take. Remember, no matter where life takes you, I will always be here for you.
+
+Enjoy your day to the fullest!
+</p>
         <p className="name">your belal</p>
       </div>
     </div>
