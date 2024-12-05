a1c86dcd08cd80cca1016f17394bc5e11956f338
Belal Uddin <139705476+coderBelal@users.noreply.github.com>
2024-12-05 23:01:07 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ const App = () => {
     <div className="birthdayCard">
       <div className="cardFront">
         <h3 className="happy">MY DEAR </h3>
-          <h4 className="happy">Siam Bhaiyaa </h4>
+          <h4 className="happy"> Mim  </h4>
        
         <div className="balloons">
           <div className="balloon-1" />
@@ -14,11 +14,11 @@ const App = () => {
       </div>
       <div className="cardInside">
         <h3 className="back"></h3>
-        <p>Dear Siam bhaieeeee</p>
+        <p>Dear  Mim</p>
         <p>
 Happy Birthday! On this special day, I want to express how grateful I am to have you in my life. You have always been my support, my confidant, and my elder brother. I cherish all the memories we've made together and look forward to many more.
 </p>
-        <p className="name">your Belal</p>
+        <p className="name">someone</p>
       </div>
     </div>
   );
